2776abcc4c1a4fe11216cba997d20be94a5a1470
Eshaan Dinesh <eshaan.dinesh@outlook.com>
2026-07-02 19:31:03 +0530

    Tokenizer, splitting, batching done

--- bigram.py
+++ bigram.py
@@ -1,7 +1,14 @@
 import torch
 
-with open(r'C:\Users\eshaa\Downloads\gpt-from-scratch\data\input.txt', 'r', encoding='utf-8') as file:
+# config
+block_size = 8
+batch_size = 4
+torch.manual_seed(1306)
+
+# data
+with open('data/input.txt', 'r', encoding='utf-8') as file:
     content = file.read()
+
 chars = sorted(set(content))
 vocab_size = len(chars)
 
@@ -19,13 +26,23 @@ n = int(0.9 * len(content))
 train_data = data[: n]
 val_data = data[n:]
 
-def get_batch(block_size):
-    x = train_data[:block_size]
-    y = train_data[1:block_size + 1]
+# testing to see the context and target
+# x = train_data[:block_size]
+# y = train_data[1:block_size + 1]
+# for t in range(block_size):
+#     context = x[:t+1]
+#     target = y[t]
+#     print(f"For input {context} target is {target}")
 
-    return (x,y)
+# batching
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    indx = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i: i + block_size] for i in indx])
+    y = torch.stack([data[i + 1: i + block_size + 1] for i in indx])
+    return x, y
 
-block_size = 8
-x,y = get_batch(block_size)
-# print(decode(x))
-print()
+# testing batch output
+x, y = get_batch('train')
+print(decode(x[0].tolist()))
+print(decode(y[0].tolist()))
