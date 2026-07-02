6d5aae7fc45162bbc3456913f4b942c05c5ec4c7
Eshaan Dinesh <eshaan.dinesh@outlook.com>
2026-07-03 01:23:15 +0530

    Created bigram model

--- bigram.py
+++ bigram.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
 
 # config
 block_size = 8
@@ -43,6 +46,70 @@ def get_batch(split):
     return x, y
 
 # testing batch output
-x, y = get_batch('train')
-print(decode(x[0].tolist()))
-print(decode(y[0].tolist()))
+# x, y = get_batch('train')
+# print(decode(x[0].tolist()))
+# print(decode(y[0].tolist()))
+
+# model
+class BigramModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        logits = self.token_embedding_table(idx)
+
+        if targets is None:
+            return logits, None
+        
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[: , -1, :]
+            probs = F.softmax(logits, dim = -1)
+            idx_next = torch.multinomial(probs, num_samples = 1)
+            idx = torch.cat((idx, idx_next), dim = 1)
+        return idx
+
+# evaluation
+@torch.no_grad()
+def estimate_loss(model, eval_iters=200):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x, y = get_batch(split)
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# training    
+model = BigramModel(vocab_size)
+optimizer = optim.AdamW(model.parameters(), lr=1e-3) 
+epochs = 10000
+
+for epoch in range(epochs):
+    if epoch % 1000 == 0:
+        stats = estimate_loss(model)
+        print(f"step {epoch}: train loss {stats['train']:.4f}, val loss {stats['val']:.4f}")
+    
+    x, y = get_batch('train')
+    logits, loss = model(x, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+# generation
+context = torch.zeros((1, 1), dtype=torch.long)
+output = model.generate(context, max_new_tokens=200)
+print(decode(output[0].tolist()))
